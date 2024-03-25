7cc38cad1ad43e99b7deb39009d2f6750313aa50
Fasil <fasilvaliyattil843@gmail.com>
2024-03-25 19:35:01 +0530

    auth seperated to auth library

--- src/app/shared/users/users.service.ts
+++ src/app/shared/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { User } from 'projects/models/src/lib/user';
+import { User } from '@@models';
 
 @Injectable({
   providedIn: 'root',
